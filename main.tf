f04188dcd9d86a69562be2f71e952ec548f147d4
Eric Stumbo <ericstumbo@student.purdueglobal.edu>
2019-10-28 23:35:04 -0400

    first

---
+++ main.tf
@@ -0,0 +1,66 @@
+provider "google-beta" {
+  project = var.project_id
+}
+
+provider "google" {
+
+}
+
+locals {
+  permis = {
+    "roles/compute.admin"          = "user:ericstumbo@student.purdueglobal.edu",
+    "roles/iam.serviceAccountUser" = "user:ericstumbo@student.purdueglobal.edu"
+  }
+}
+
+resource "google_project_iam_member" "project" {
+  project  = var.project_id
+  for_each = local.permis
+  role     = each.key
+  member   = each.value
+}
+
+resource "google_compute_firewall" "default" {
+  project       = var.project_id
+  name          = "allow-ssh-from-iap"
+  network       = "default"
+  source_ranges = ["35.235.240.0/20"]
+  allow {
+    protocol = "tcp"
+  }
+}
+
+## Have to add a local-exec to delete default SSH rule
+
+data "google_compute_image" "my_image" {
+  family  = "debian-9"
+  project = "debian-cloud"
+}
+
+resource "google_compute_instance" "default" {
+  project      = "${var.project_id}"
+  machine_type = "n1-standard-1"
+  name         = "${var.instance_name}"
+  zone         = "${var.zone}"
+  network_interface {
+    network = "default"
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "${data.google_compute_image.my_image.self_link}"
+    }
+  }
+}
+
+resource "google_iap_tunnel_instance_iam_member" "instance" {
+  provider = "google-beta"
+  instance = "${var.instance_name}"
+  zone     = "${var.zone}"
+  role     = "roles/iap.tunnelResourceAccessor"
+  member   = "user:ericstumbo@student.purdueglobal.edu"
+  depends_on = [google_compute_instance.default]
+}
+
+
+
---
+++ vars.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+    description = "project ID"
+    default = ""
+}
+
+variable "instance_name" {
+   description = "VM name"
+   default  = "iap-demo"
+}
+variable "zone" {
+   description = "project zone"
+   default  = "us-central1-a"
+}
+
